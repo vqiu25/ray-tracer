2fc9b22920749439bbb550aa1bc746df3d5753ce
victor <vqiu25@gmail.com>
2024-05-12 13:13:53 +1200

    Functionality: Implemented vector class

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,8 @@
 
 int main() {
 
-  // Image
-
   int image_width = 256;
   int image_height = 256;
-
-  // Render
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
   sdltemplate::sdl("Ray Tracer", image_width, image_height);
