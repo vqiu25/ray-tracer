d751846911b660e7d2c6b9247236199cb587dcd2
victor <vqiu25@gmail.com>
2024-05-13 21:33:49 +1200

    Functionality: Added timer

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "scene.h"
 #include "sdltemplate.h"
 #include "vec3.h"
+#include <chrono>
 #include <iostream>
 
 int main() {
@@ -9,6 +10,11 @@ int main() {
   // 4:2 Aspect Ratio
   int imageWidth = 800;
   int imageHeight = 400;
+
+  // Print to terminal
+  std::cout << "Format: P3 (colors in ASCII)\n";
+  std::cout << "Dimensions: " << imageWidth << " x " << imageHeight << '\n';
+  std::cout << "Max Color Value: 255\n";
 
   /*
   Visual Representation:
@@ -28,9 +34,11 @@ int main() {
   vec3 sphereCentre(0, 0, -1);
   double sphere_radius = 0.5;
 
-  std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
   sdltemplate::sdl("Ray Tracer", imageWidth, imageHeight);
   sdltemplate::loop();
+
+  // Start timer
+  auto start = std::chrono::high_resolution_clock::now();
 
   for (int j = 0; j < imageHeight; j++) {
     for (int i = 0; i < imageWidth; i++) {
@@ -46,6 +54,16 @@ int main() {
       sdltemplate::drawPoint(i, imageHeight - j);
     }
   }
+
+  // Stop timer
+  auto stop = std::chrono::high_resolution_clock::now();
+
+  // Calculate duration
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+  std::cout << "Time taken for rendering: " << duration.count()
+            << " milliseconds\n";
 
   while (sdltemplate::running) {
     sdltemplate::loop();
